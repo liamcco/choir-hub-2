0c70ff8ab117d658567a1436515fda55fdc8ef65
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2026-06-10 21:15:29 +0200

    fix signout button

--- src/components/auth/sign-out-button.tsx
+++ src/components/auth/sign-out-button.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import { Spinner } from '@/components/ui/spinner';
-import { useActionState } from 'react';
-
 import { Button } from '@/components/ui/button';
-import { signOutAction } from '@/lib/auth-actions';
+import { Spinner } from '@/components/ui/spinner';
 import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export function SignOutButton() {
-  const [, action, isSigningOut] = useActionState(signOutAction, null);
-  const { data, isPending } = authClient.useSession();
+  const router = useRouter();
+  const [isSigningOut, setIsSigningOut] = useState(false);
+  const { data, isPending: isLoadingSession } = authClient.useSession();
 
-  if (isPending) {
+  if (isLoadingSession) {
     return (
       <Button variant="default" disabled>
         <Spinner />
@@ -20,26 +20,34 @@ export function SignOutButton() {
     );
   }
 
+  const handleSignOut = async () => {
+    setIsSigningOut(true);
+
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.refresh();
+        },
+      },
+    });
+
+    setIsSigningOut(false);
+  };
+
   if (!data) {
-    return (
-      <Link href="/login">
-        <Button variant="default">Sign in</Button>
-      </Link>
-    );
+    return <Button variant="default" render={<Link href="/login">Sign in</Link>}></Button>;
   }
 
   return (
-    <form action={action}>
-      <Button aria-busy={isSigningOut} disabled={isSigningOut} variant="default" type="submit">
-        {isPending ? (
-          <>
-            <Spinner />
-            <span>Signing out...</span>
-          </>
-        ) : (
-          'Sign out'
-        )}
-      </Button>
-    </form>
+    <Button aria-busy={isSigningOut} disabled={isSigningOut} variant="default" type="button" onClick={handleSignOut}>
+      {isSigningOut ? (
+        <>
+          <Spinner />
+          <span>Signing out...</span>
+        </>
+      ) : (
+        'Sign out'
+      )}
+    </Button>
   );
 }
