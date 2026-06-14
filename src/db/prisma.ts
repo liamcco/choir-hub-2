35eaf1bdc9d8664dc5dcb7baf06012833f64f02f
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2026-06-14 12:01:50 +0200

    add crt

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -3,6 +3,16 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { isProduction } from '@/common/environment/environment'
 import { PrismaClient } from '@/prisma/generated/client'
 
+import fs from 'node:fs'
+
+import path from 'node:path'
+
+const ca = fs.readFileSync(
+  path.join(process.cwd(), 'src/db/prod-ca-2021.crt'),
+
+  'utf8',
+)
+
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
 const logPrismaQueries = process.env.LOG_PRISMA === 'true'
@@ -14,7 +24,8 @@ export const prisma =
     adapter: new PrismaPg({
       connectionString: process.env.POSTGRES_PRISMA_URL,
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
+        ca,
       },
     }),
   })
